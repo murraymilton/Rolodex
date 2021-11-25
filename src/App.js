36cc055fca7953132b5a3a5c12b06e4b5042d561
murraymilton <murray.l.milton@outlook.com>
2021-11-25 18:00:32 +0100

    testing the child prop between the component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-
-import logo from "./logo.svg";
+import { CardList } from "./components/card-list/card-list.component";
 import "./App.css";
 
 class App extends Component {
@@ -8,24 +7,24 @@ class App extends Component {
     super();
 
     this.state = {
-      string: "Hello Rolodex! How may I assist you?",
+      monsters: [],
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users").then((response) =>
+      response.json().then((users) => this.setState({ monsters: users }))
+    );
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.string}</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <CardList>
+          <h1>Jeff Sauerbocker</h1>
+        </CardList>
+        {this.state.monsters.map((monsters) => (
+          <h1 key={monsters.id}>{monsters.name}</h1>
+        ))}
       </div>
     );
   }
