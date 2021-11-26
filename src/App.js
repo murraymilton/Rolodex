51205a631531bb3bf85b071b9e38de60116e35d0
murraymilton <murray.l.milton@outlook.com>
2021-11-26 17:44:57 +0100

    added the card component for displaying rolodex info

--- src/App.js
+++ src/App.js
@@ -19,12 +19,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CardList>
-          <h1>Jeff Sauerbocker</h1>
-        </CardList>
-        {this.state.monsters.map((monsters) => (
-          <h1 key={monsters.id}>{monsters.name}</h1>
-        ))}
+        <CardList monsters={this.state.monsters}></CardList>
       </div>
     );
   }
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { Card } from "./card/card.component";
+import "./card-list.styles.css";
 
 export const CardList = (props) => {
-  return <div>{props.children}</div>;
+  return (
+    <div className="card-list">
+      {props.monsters.map((monsters) => (
+        <Card key={monsters.id} monsters={monsters} />
+      ))}
+    </div>
+  );
 };
